c6512a4ae51e262440aecd3f8f7443c63a970171
NightMeh <99148447+NightMeh@users.noreply.github.com>
2023-01-16 12:18:43 +0000

    ADDED GUI

--- gui.py
+++ gui.py
@@ -1,4 +1,29 @@
-from PyQt5 import QtGui
-class Gui(QApplication):
-    def __init__(self, *args, **kwargs):
-        
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+app = QtWidgets.QApplication([])
+
+widget = MyWidget()
+widget.resize(800, 600)
+widget.show()
+
+sys.exit(app.exec())
